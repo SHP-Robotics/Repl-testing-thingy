9556be218636831ef44903b46dc42146acfcd8af
kshen23 <68457455+kshen23@users.noreply.github.com>
2020-07-25 22:45:20 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("Earth to github");
+    System.out.println("Earth to replit");
   }
 }
