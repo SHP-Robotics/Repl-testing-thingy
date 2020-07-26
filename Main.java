7fcf047b32eb23641f82a4b6f3ddf43d37728d5b
Lucy Knox <lknox23@shschools.org>
2020-07-26 05:47:01 +0000

    Lucy to GitHub

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main {
     System.out.println("Hello world!");
     System.out.println("Earth to github");
     System.out.println("Earth to replit");
+    System.out.println("Lucy to github")
   }
 }
